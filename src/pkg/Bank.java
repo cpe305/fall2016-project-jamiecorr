eec150b09a3b50fb377cb9168331c319f925a7bf
Jamie Corr <jamiecorr4@gmail.com>
2016-10-09 18:38:36 -0700

    Bank setup

--- src/pkg/Bank.java
+++ src/pkg/Bank.java
@@ -1,5 +1,21 @@
 package pkg;
 public class Bank
 {
+    private double money;
+    
+    public Bank(double money)
+    {
+    	this.money = money;
+    }
+     
+    public void addMoney(double amount)
+    {
+    	money += amount;
+    }
+     
+    public void subtractMoney(double amount)     
+    {
+    	money -= amount;
+    }
 
 }
