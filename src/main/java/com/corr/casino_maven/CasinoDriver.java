0be63cf05ede4ecd2a8998592469e353157562b9
Jamie Corr <jamiecorr4@gmail.com>
2016-10-13 19:04:10 -0700

    Editing driver after testing

--- src/main/java/com/corr/casino_maven/CasinoDriver.java
+++ src/main/java/com/corr/casino_maven/CasinoDriver.java
@@ -1,49 +1,44 @@
 package com.corr.casino_maven;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class CasinoDriver {
 	public static final void main(String[] args) {
-//		DeckTest.testCreateDeck();
-		
-		
-		
-		
-//		double startingBalance = 100;
-//		Bank playersBank = new Bank(startingBalance);
-//	     
-//		System.out.println("Do you want to play in the casino? (yes or no)");
-//		Scanner scan = new Scanner(System.in);
-//		String input = scan.nextLine();
-//       
-//		while (input.equals("yes"))
-//		{
-//           System.out.println("You have $" + playersBank.getCurrentBalance()+ "Press 1 for Blackjack or 2 for Poker");
-//           int gameChoice = scan.nextInt();
-//           
-//           if (gameChoice == 1)
-//           {
-//               Blackjack.play(playersBank);
-//           }      
-//           else if (gameChoice == 2)
-//           {
-//               Poker.play(playersBank);
-//           }
-//           
-//           if (!playersBank.isBroke())
-//           {
-//               System.out.println("Do you want to stay in the casino?");
-//               input = scan.nextLine();
-//           }
-//           else
-//           {
-//               System.out.println("No money left, now exiting casino.");
-//        	   break;
-//           }
-//		}
-//	       
-//		System.out.println("---exit---");
-//		scan.close();
+		double startingBalance = 100;
+		Bank playersBank = new Bank(startingBalance);
+	     
+		System.out.println("Do you want to play in the casino? (yes or no)");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+       
+		while (input.equals("yes"))
+		{
+           System.out.println("You have $" + playersBank.getCurrentBalance()+ "Press 1 for Blackjack or 2 for Poker");
+           int gameChoice = scan.nextInt();
+           
+           if (gameChoice == 1)
+           {
+               Blackjack.play(playersBank);
+           }      
+           else if (gameChoice == 2)
+           {
+               Poker.play(playersBank);
+           }
+           
+           if (!playersBank.isBroke())
+           {
+               System.out.println("Do you want to stay in the casino?");
+               input = scan.nextLine();
+           }
+           else
+           {
+               System.out.println("No money left, now exiting casino.");
+        	   break;
+           }
+		}
+	       
+		System.out.println("---exit---");
+		scan.close();
 	}
 }
 
