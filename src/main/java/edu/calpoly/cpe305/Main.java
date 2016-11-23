47e2740b4f9c4ea3edbbb991eb2e9a94443ceef7
Jamie Corr <jamiecorr4@gmail.com>
2016-11-23 11:45:02 -0800

    sonar4

--- src/main/java/edu/calpoly/cpe305/Main.java
+++ src/main/java/edu/calpoly/cpe305/Main.java
@@ -9,16 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws MyOwnRuntimeException {
-    try {
-      BorderPane root = new BorderPane();
-      Scene scene = new Scene(root, 400, 400);
-      scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-      primaryStage.setScene(scene);
-      primaryStage.show();
-    } catch (Exception exc) {
-      exc.printStackTrace();
-      throw new MyOwnRuntimeException("My Message");
-    }
+    BorderPane root = new BorderPane();
+    Scene scene = new Scene(root, 400, 400);
+    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
